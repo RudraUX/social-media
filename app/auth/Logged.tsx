a9c6bab6903c302850547d55bf83a6587bf6059a
RudraUX <pollabrock@gmail.com>
2023-02-23 11:10:55 +0600

    ✨bug fix

--- app/auth/Logged.tsx
+++ app/auth/Logged.tsx
@@ -12,23 +12,24 @@ import Link from 'next/link';
 export default function Logged({ user }: any) {
   return (
     <li className='flex gap-8 items-center'>
-      <Link href='/dashboard'>
-        <a>
-          <Image
-            src={user.image}
-            alt={user.name}
-            width={40}
-            height={40}
-            className='rounded-full'
-          />
-        </a>
-      </Link>
       <button
         className='text-sm bg-gray-700 text-white py-2 px-6 rounded-xl disabled:opacity-25'
         onClick={() => signOut()}
       >
         Sign out
       </button>
+      {/* <Link href='/dashboard'>
+        <a>
+          <Image
+            src={user.image}
+            alt={user.name}
+            width={40}
+            height={40}
+            className='rounded-full w-14'
+            priority
+          />
+        </a>
+      </Link> */}
     </li>
   );
 }
